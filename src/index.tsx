fd2b155c3c941df6fe3255fcc8f654c8b3923bf9
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-12-15 05:55:44 +0000

    test ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import Parent from "./props/Parent"
 
 const App = () => {
-  return( <Parent>Hi, there!</Parent>)
+  return(
+    <Parent />
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
