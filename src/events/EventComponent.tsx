6f5d06910cf978542f20c1c046a1fa0d81d32082
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-08-21 20:52:17 +0000

    learn again

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const EventComponent:React.VFC = () => {
     const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {console.log(event)}
+    const onDragStart = (event: React.DragEvent<HTMLDivElement> ) => {console.log("im being dragged")}
+    //    const onDragStart = (event ) => {
+    //      console.log("im being dragged");
+    //    };
+ 
     return (
         <div>
             <input onChange={onChange} />
+            <div draggable onDragStart={onDragStart}>Drag me!</div>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import GuestList from "./state/GuestList";
+import GuestList from "./state/GuestList";
 // import UserSearch from "./state/UserSearch";
-import EventComponent from "./events/EventComponent"
+// import EventComponent from "./events/EventComponent"
 
 const App = () => {
   return(
     <div>
-    <EventComponent />
+    <GuestList />
     </div>
   )
 }
